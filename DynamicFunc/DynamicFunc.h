41c1f551e7238895a9510d170223bef3416f3767
dyexlzc <292856273@qq.com>
2019-12-14 16:39:08 +0000

    增加了DynamicFunc模块，用于动态调用函数，输入参数并且返回结果。是RPC的核心模块

--- DynamicFunc/DynamicFunc.h
+++ DynamicFunc/DynamicFunc.h
@@ -15,7 +15,7 @@ public:
         //定义类和参数so文件的存放位置
     }
     //因为DF模块面对的是来自网络上的数据，因此必须有一种有效的方法来从比特流中重构数据
-    void Call(std::string FuncName,char* Param){
+    void Call(std::string FuncName,char* buffer,char* returns){
         //1.通过FuncName动态加载Func所对应的Class以及Param所对应的Class
         //2.因此，客户端和服务器必须有一份同样的Class来描述参数的数据结构
 
@@ -23,8 +23,10 @@ public:
         std::string FuncSoName=FuncName+".so";
         loader.load(FuncSoName);
         interface *p=loader.getInstance(FuncSoName);        //取得函数的实例
-        //2.现在函数已经取出来，需要再取出函数需要的参数列表
-        
+        //2.现在函数已经取出来，只需要把buffer填入run的参数(buffer)中即可，计算和对buffer的操作将在run中由用户手动memcpy格式化到struct/class中
+        p->run(buffer,returns);
+        //由so内部将结果存放到returns中, 指针returns是一个由调用者按照so中的调用规范所申请的一个内存地址。
+        //因此计算完成后，returns的内存地址中存放的就是计算结果了
     }
 };
 #endif
---
+++ DynamicFunc/test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include"DynamicFunc.h"
+/*
+    动态加载so调用函数并且返回结果示例
+*/
+using namespace std;
+struct param_Add{
+    int a,b;
+};
+int main(){
+    DynamicFunc df("../DynamicSo/");//指定so文件的位置
+    int result;             //申请内存保存结果
+    struct param_Add pa;    //和服务端保持一致的参数格式
+    cin>>pa.a>>pa.b;
+    df.Call("add",(char*)&pa,(char*)&result);   //远程调用函数
+    cout<<result;
+    return 0;
+} 
